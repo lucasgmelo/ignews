aa58610d56fb869da1fe472ccb36deffaeb95ab6
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-15 02:54:49 -0300

    :giraffe: fix not saving subscription on db bug

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,6 +1,6 @@
-import { fauna } from "../../../services/fauna";
-import { query } from "faunadb";
-import { stripe } from "../../../services/stripe";
+import { fauna } from '../../../services/fauna'
+import { query } from 'faunadb'
+import { stripe } from '../../../services/stripe'
 
 export async function saveSubscription(
   subscriptionId: string,
@@ -9,41 +9,41 @@ export async function saveSubscription(
 ) {
   const userRef = await fauna.query(
     query.Select(
-      "ref",
+      'ref',
       query.Get(
-        query.Match(query.Index("user_by_stripe_customer_id"), customerId)
+        query.Match(query.Index('user_by_stripe_customer_id'), customerId)
       )
     )
-  );
+  )
 
-  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
 
   const subscriptionData = {
     id: subscription.id,
     userId: userRef,
     status: subscription.status,
     price_id: subscription.items.data[0].price.id,
-  };
+  }
+
+  console.log(subscriptionData)
 
   if (createAction) {
     await fauna.query(
-      query.Create(query.Collection("subscriptions"), {
+      query.Create(query.Collection('subscriptions'), {
         data: subscriptionData,
       })
-    );
+    )
   } else {
-      await fauna.query(
-          query.Replace(
-              query.Select(
-                  "ref",
-                  query.Get(
-                      query.Match(
-                          query.Index("subscription_by_id"),
-                          subscriptionId,
-                      )
-                  )
-              ), { data: subscriptionData }
+    await fauna.query(
+      query.Replace(
+        query.Select(
+          'ref',
+          query.Get(
+            query.Match(query.Index('subscription_by_id'), subscription.id)
           )
+        ),
+        { data: subscriptionData }
       )
+    )
   }
 }
