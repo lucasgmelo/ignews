b7cdcc57cca77f69583473278d7d57ef93cdc310
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-14 23:28:31 -0300

    :sauropod: SSR stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
@@ -36,9 +36,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve("price_1IgKeJLaqivZ1wMJCQFN97wS", {
-    expand: ["product"],
-  });
+  const price = await stripe.prices.retrieve("price_1IgKeJLaqivZ1wMJCQFN97wS");
 
   const product = {
     priceId: price.id,
