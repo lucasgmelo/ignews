67888b415ee73f3beebb59a702b784c2469b0509
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-15 03:55:54 -0300

    :sauropod: create posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Link from 'next/link'
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
 import styles from "./styles.module.scss";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -9,23 +25,53 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.postsList}>
-          <a>
-            <time>dataaa</time>
-            <strong>T'ituloooo</strong>
-            <p>paragraphhh</p>
-          </a>
-          <a>
-            <time>dataaa</time>
-            <strong>T'ituloooo</strong>
-            <p>paragraphhh</p>
-          </a>
-          <a>
-            <time>dataaa</time>
-            <strong>T'ituloooo</strong>
-            <p>paragraphhh</p>
-          </a>
+          {posts.map((post) => (
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
