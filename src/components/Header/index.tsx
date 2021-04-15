ffd5784d9e48360d60301ae1cf3abda13ad51988
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-15 04:01:17 -0300

    :sauropod: create and style navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 import styles from './styles.module.scss'
 
 export function Header() {
+
     return (
-        <header className={styles.headerContainer}>
+        <header className={styles.headerContainer}> 
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ignews"/>
-                <nav>
-                    <a className={styles.active}>Home</a>
+              <img src="/images/logo.svg" alt="ig.news" />
+              <nav>
+                  <ActiveLink activeClassName={styles.active} href="/">
+                    <a>Home</a>
+                  </ActiveLink>
+                  <ActiveLink activeClassName={styles.active} href="/posts">
                     <a>Posts</a>
-                </nav>
-                <SignInButton />
+                  </ActiveLink>
+              </nav>
+              <SignInButton />
             </div>
         </header>
     )
