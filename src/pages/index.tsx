bbb8d114654b8f5aff727103c723cc0c0c76398e
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-04-14 22:59:08 -0300

    :sauropod: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import Head from 'next/head'
+import Head from "next/head";
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>início | ignews</title>
+        <title>home | ignews</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>✨ Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
-  )
+  );
 }
