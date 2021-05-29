c6c31ef94af4fa10bb4ae2f81ec2a9490a9272fa
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-05-29 16:58:48 -0300

    :giraffe: fix jest config bug

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: [
     "<rootDir>/src/tests/setupTests.ts"
   ],
   transform: {
-    "ˆ.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
-};
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy"
+  }
+}
